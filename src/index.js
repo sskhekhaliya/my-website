901a74da9ff4fecb3df09e82808b9ab03fd928bf
Saurav Singh Khekhaliya <sskhekhaliya@gmail.com>
2025-09-03 14:04:21 +0530

    Replaced react-helmet-async with @dr.pogodin/react-helmet for React 19 compatibility

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "@dr.pogodin/react-helmet";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
